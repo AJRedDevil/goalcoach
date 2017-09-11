6ff3be6dc97832e165023768eee2120062439769
AJRedDevil <ajan.shresh@gmail.com>
2017-09-11 20:30:20 +0545

    Added the react router and 3 paths

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Switch } from 'react-router';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import App from './App.jsx';
 import SignIn from './SignIn.jsx';
 import SignUp from './SignUp.jsx';
 
 ReactDOM.render(
-    <div>App</div>,
+    <Router path="/">
+        <Switch>
+            <Route path="/app" component={App} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+        </Switch>
+    </Router>,
     document.getElementById('root')
 );
