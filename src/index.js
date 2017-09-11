5f0d6445a1bdaf93d3d9fff17c6cf77043641b1d
AJRedDevil <ajan.shresh@gmail.com>
2017-09-11 20:18:44 +0545

    Initial repo

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App.jsx';
+import SignIn from './SignIn.jsx';
+import SignUp from './SignUp.jsx';
+
+ReactDOM.render(
+    <div>App</div>,
+    document.getElementById('root')
+);
