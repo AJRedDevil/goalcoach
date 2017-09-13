c5240e3e139c96d5b7fcf8d1ba53a6a1ee5425a4
AJRedDevil <ajan.shresh@gmail.com>
2017-09-13 20:21:27 +0545

    Sign in sign out firebase

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { firebaseApp } from '../firebase.js'
 
 export default class App extends Component {
+    signOut = () => {
+        firebaseApp.auth().signOut();
+    }
     render() {
         return (
             <div>
-                App
+                <button
+                    className="btn btn-danger"
+                    onClick={() => this.signOut()}
+                >
+                    Sign Out
+                </button>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Switch } from 'react-router';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import { firebaseApp } from './firebase';
 
 import App from './components/App.jsx';
 import SignIn from './components/SignIn.jsx';
 import SignUp from './components/SignUp.jsx';
+import history from './history';
 
 firebaseApp.auth().onAuthStateChanged(user => {
     if (user) {
         console.log('user has signed in or up', user);
+        history.push('/app');
     } else {
         console.log('user has signed out or still needs to sign in.');
+        history.push('/signin');
     }
 })
 
 ReactDOM.render(
-    <Router path="/">
+    <Router path="/" history={history}>
         <Switch>
             <Route path="/app" component={App} />
             <Route path="/signin" component={SignIn} />
